f672a248a83451cf86a790f274857a9cb471fc66
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-08 20:54:18 +0100

    Set up home routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeaturesModule } from './features/features.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+
+    FeaturesModule,
+
+    NgbModule, // Bootstrap
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
