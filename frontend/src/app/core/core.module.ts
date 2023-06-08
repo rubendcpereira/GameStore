6418f7a10013fae53d60ed13d347c34c57b03d42
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-09 00:45:48 +0100

    Import SharedModule

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class CoreModule {}
