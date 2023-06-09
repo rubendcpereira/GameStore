68423d28aa0b3f5c7e49cafc7b1f639e886828d9
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-09 01:40:51 +0100

    Add header with navbar to be completed

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './layout/header/header.component';
 import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
-  declarations: [HeaderComponent],
-  imports: [CommonModule, SharedModule],
+  declarations: [HeaderComponent, NavbarComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
+  exports: [HeaderComponent],
 })
 export class CoreModule {}
