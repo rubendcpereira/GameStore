8396bd7a3f7492139d6cddb8f1ac4507150eb08c
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-16 00:39:51 +0100

    Add home page

--- frontend/src/app/features/home/pages/home/home.component.ts
+++ frontend/src/app/features/home/pages/home/home.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game } from 'src/app/core/models/game.model';
+import { GameService } from 'src/app/core/services/game.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  games$!: Observable<Game[]>;
+
+  constructor(private gameService: GameService) {}
+
+  ngOnInit(): void {
+    this.games$ = this.getGames();
+  }
+
+  getGames(): Observable<Game[]> {
+    return this.gameService.getGames();
+  }
+}
