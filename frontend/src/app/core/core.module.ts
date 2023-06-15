243c7e009d6ed1ef2c210fb79333f8f1b95136a8
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-15 18:13:38 +0100

    Add api interceptor

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './layout/navbar/navbar.component';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule } from '@angular/router';
+import { ApiInterceptor } from './interceptors/api.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [NavBarComponent],
-  imports: [
-    CommonModule,
-    RouterModule,
-    
-    SharedModule
-  ],
+  imports: [CommonModule, RouterModule, HttpClientModule, SharedModule],
   exports: [NavBarComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {}
---
+++ frontend/src/app/core/interceptors/api.interceptor.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  private baseUrl: string = 'http://localhost:3000';
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request.clone({ url: this.baseUrl + request.url }));
+  }
+}
