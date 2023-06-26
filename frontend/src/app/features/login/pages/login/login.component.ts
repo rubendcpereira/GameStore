09bcc32d30312dbb45d6347d295a6ca04140aa08
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-26 17:59:12 +0100

    Add login page logic (without validation yet)

--- frontend/src/app/features/login/pages/login/login.component.ts
+++ frontend/src/app/features/login/pages/login/login.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  public loginForm = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) {}
+
+  public onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { username, password } = this.loginForm.value;
+      this.authService.login(username!, password!);
+    }
+  }
+}
