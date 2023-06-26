e7dee0a85710020a007ae19a571e386ecc384da9
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-26 06:07:16 +0100

    Implement login method

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
+const SECRET_KEY = "secretKey";
+
 exports.register = async (req, res, next) => {
   try {
     const user = new User(req.body);
@@ -13,8 +15,24 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  // TODO
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+
+    // Validation
+    if (!user) {
+      return res.sendStatus(404);
+    } else if (!(await bcrypt.compare(req.body.password, user.password))) {
+      return res.sendStatus(400);
+    }
+
+    // Response
+    const token = jwt.sign({ _id: user._id }, SECRET_KEY);
+    const { password, ...data } = user.toJSON();
+    res.cookie("jwt", token, { maxAge: 24 * 60 * 60 * 1000 /* 1 day */ }).send(data);
+  } catch (err) {
+    return next(err);
+  }
 };
 
 exports.logout = (req, res, next) => {
