884b820f9c8fd12be839de5fa28d80f184f015e4
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-26 17:58:40 +0100

    Add missing module imports (core and shared)

--- frontend/src/app/features/login/login.module.ts
+++ frontend/src/app/features/login/login.module.ts
@@ -3,9 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './pages/login/login.component';
+import { CoreModule } from 'src/app/core/core.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [CommonModule, LoginRoutingModule],
+  imports: [
+    CommonModule,
+
+    CoreModule,
+    SharedModule,
+    
+    LoginRoutingModule,
+  ],
 })
 export class LoginModule {}
