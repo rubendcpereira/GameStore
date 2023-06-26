71532a4eda40e0fa5d841276ba2af2edce6f2f44
rubendcpereira <rubendanielpereira18@gmail.com>
2023-06-26 18:46:35 +0100

    Adapt navbar display according to the user's authentication

--- frontend/src/app/core/layout/navbar/navbar.component.ts
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
@@ -6,10 +8,32 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
 })
-export class NavBarComponent {
-  public isMenuCollapsed = true;
+export class NavBarComponent implements OnInit {
+  public isMenuCollapsed: boolean = true;
+  public isUserLoggedIn: boolean = false;
+  private readonly ngUnsubscribe: Subject<void> = new Subject<void>();
 
-  constructor(private authService: AuthService) {}
+  constructor(private router: Router, private authService: AuthService) {}
+
+  public ngOnInit(): void {
+    this.isLoggedIn();
+  }
+
+  public ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
+  public isLoggedIn(): void {
+    this.authService
+      .isLoggedIn()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((isUserLoggedIn) => (this.isUserLoggedIn = isUserLoggedIn));
+  }
+
+  public login() {
+    this.router.navigate(['/login']);
+  }
 
   public logout() {
     this.authService.logout();
